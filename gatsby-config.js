345625bc388aa081916497bea173acf2621ae745
Ali Heydari <ali4heydari@gmail.com>
2020-09-20 18:31:57 +0430

    chore: add i18n package and react hot loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,38 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const faTranslations = require("./src/locales/fa/translations.json");
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  pathPrefix: `/`,
+  siteMetadata: {
+    title: faTranslations["website_name"],
+    description: faTranslations["website_description"],
+    author: "@ali4heydari",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#FF54AC`,
+    //     display: `minimal-ui`,
+    //     icon: `${__dirname}/static/images/histaff.png`, // This path is relative to the root of the site.
+    //   },
+    // },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
