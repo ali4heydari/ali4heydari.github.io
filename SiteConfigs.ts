bf30e067628530711870023efdd4464b4b56a3b7
Ali Heydari <ali4heydari@gmail.com>
2020-09-20 18:41:10 +0430

    feat: add site config file

---
+++ SiteConfigs.ts
@@ -0,0 +1,12 @@
+export default {
+  FACEBOOK_ID: "",
+  TWITTER_ID: "",
+  INSTAGRAM_ID: "",
+  LINKEDIN_ID: "",
+  CONTACT_EMAIL: "ali4heydari@gmail.com",
+  CONTACT_PHONE: "+98 936 856 3509",
+  SHORT_NAME: "site short name",
+  SITE_TITLE: "site_title",
+  SITE_DESCRIPTION: "site_description",
+  SITE_AUTHOR: "site_author",
+};
