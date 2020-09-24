9ba1d851f58909515ba8b759245a7fe8de6cfa17
Ali Heydari <ali4heydari@gmail.com>
2020-09-24 22:42:47 +0330

    feat(componets): 🔎  use seo in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,8 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n/config";
 import { Footer } from "./Footer";
 import "./Layout.css";
+import { Seo } from "./Seo";
+import SiteConfigs from "../../SiteConfigs";
 
 export const Layout = ({ children, showHeader = true }) => {
   const data = useStaticQuery(graphql`
@@ -21,6 +23,7 @@ export const Layout = ({ children, showHeader = true }) => {
   return (
     <I18nextProvider i18n={i18n}>
       <Theme>
+        <Seo title={SiteConfigs.SITE_TITLE} />
         <main>{children}</main>
         <Footer />
       </Theme>
