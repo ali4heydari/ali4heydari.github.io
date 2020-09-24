bf1b3f100060997a71db7fabffdeb751aab6d3ee
Ali Heydari <ali4heydari@gmail.com>
2020-09-24 22:23:13 +0330

    fix(pages): 🛠 remove unresolved tailwind class in 404 page to pass build

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -16,7 +16,7 @@ export default function P404() {
             But dont worry, you can find plenty of other things on our homepage.
           </p>
           <button
-            css={tw`px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700`}
+            css={tw`px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none bg-blue-600 active:bg-blue-600 hover:bg-blue-700`}
           >
             back to homepage
           </button>
