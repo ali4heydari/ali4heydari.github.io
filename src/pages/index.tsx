c263a07f5d73a733d185f9506738aa6bb62a41c1
Ali Heydari <ali4heydari@gmail.com>
2020-09-24 21:31:10 +0330

    feat(pages): 🧂 use componets in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Layout } from "../components/Layout";
+import { AnimationRevealPage } from "../components/AnimationRevealPage";
+import { Hero } from "../components/Hero";
+import { Skills } from "../components/skills";
+import { ContactMe } from "../components/ContactMe";
 
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <Layout>
+      <AnimationRevealPage>
+        <Hero />
+        <Skills />
+
+        <ContactMe />
+      </AnimationRevealPage>
+    </Layout>
+  );
 }
