5bddce83d3b6c54fca2071511f001e7c6624b189
Ali Heydari <ali4heydari@gmail.com>
2020-09-24 22:27:09 +0330

    fix(pages): 🔨 fix import in home page that cuz failing build in CI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Layout } from "../components/Layout";
 import { AnimationRevealPage } from "../components/AnimationRevealPage";
 import { Hero } from "../components/Hero";
-import { Skills } from "../components/skills";
+import { Skills } from "../components/Skills";
 import { ContactMe } from "../components/ContactMe";
 
 export default function Home() {
