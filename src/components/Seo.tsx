9511f6033effa1adac8c0bb6e38a98bc13d22072
Ali Heydari <ali4heydari@gmail.com>
2020-09-24 21:28:41 +0330

    feat(componets): 🔍 add seo component

---
+++ src/components/Seo.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql, useStaticQuery } from "gatsby";
+import { useTranslation } from "react-i18next";
+import { Languages, StringKeys } from "../utils/enums";
+
+export function Seo({ description = "", lang = "fa", meta = [], title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  );
+  const { i18n, t } = useTranslation();
+  const metaDescription = description || site.siteMetadata.description;
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang:
+          i18n.language === Languages.CI_MODE
+            ? Languages.ENGLISH
+            : i18n.language,
+        dir: i18n.language === Languages.PERSIAN ? "rtl" : "ltr",
+      }}
+      title={title}
+      titleTemplate={`%s | ${t(StringKeys.WEBSITE_NAME)}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: `${title} | ${t(StringKeys.WEBSITE_NAME)}`,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: `${title} | ${t(StringKeys.WEBSITE_NAME)}`,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  );
+}
