dfef9202bbc47356118fb7f869bde2987ac9b8d9
Ali Heydari <ali4heydari@gmail.com>
2020-09-24 22:42:47 +0330

    feat(componets): 🔎  use seo in layout

--- SiteConfigs.ts
+++ SiteConfigs.ts
@@ -6,7 +6,7 @@ export default {
   CONTACT_EMAIL: "ali4heydari@gmail.com",
   CONTACT_PHONE: "+98 936 856 3509",
   SHORT_NAME: "site short name",
-  SITE_TITLE: "site_title",
+  SITE_TITLE: "Ali Heydari's protfolio",
   SITE_DESCRIPTION: "site_description",
   SITE_AUTHOR: "site_author",
 };
