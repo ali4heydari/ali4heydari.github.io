ef2972e9fe4baf84b932fd76285bc31574fd4230
Ali Heydari <ali4heydari@gmail.com>
2020-09-25 16:31:09 +0330

    fix(pages): 💺 add seo component to home page in order to set meanningful title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Layout } from "../components/Layout";
 import { AnimationRevealPage } from "../components/AnimationRevealPage";
 import { Hero, Skills, ContactMe, Projects } from "../components";
+import { Seo } from "../components/Seo";
 
 export default function Home() {
   return (
     <Layout>
+      <Seo title={"Home"} />
       <AnimationRevealPage>
         <Hero />
         <Skills />
