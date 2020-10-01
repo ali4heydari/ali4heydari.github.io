8b60eb503d595747ea3070347dcf88ed23136ac9
Ali Heydari <ali4heydari@gmail.com>
2020-10-01 19:30:18 +0330

    fix(global): 🔨 add global as prop to react components

--- global.d.ts
+++ global.d.ts
@@ -10,6 +10,8 @@ import { CSSProp } from "styled-components";
 declare module "react" {
   interface Attributes {
     css?: CSSProp;
+    // https://github.com/emotion-js/emotion/issues/1137#issuecomment-615571356
+    as?: React.ElementType | keyof JSX.IntrinsicElements;
   }
   declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
