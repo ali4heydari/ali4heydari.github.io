551d7ebc4a93be0612dd2bd2ce35352c382cfbca
Ali Heydari <ali4heydari@gmail.com>
2020-10-01 11:20:37 +0330

    feat(componets): ⌚ add timeline componet

--- src/components/misc/Typography.tsx
+++ src/components/misc/Typography.tsx
@@ -1,2 +1,3 @@
 import tw from "twin.macro";
 export const SectionDescription = tw.p`mt-4 text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100 max-w-xl`;
+export const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
