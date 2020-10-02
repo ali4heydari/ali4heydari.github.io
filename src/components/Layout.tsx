7741d03817128a3e6820f7df6fa5ce38981be23e
Ali Heydari <ali4heydari@gmail.com>
2020-10-02 14:58:25 +0330

    refactor(components): 🧹 remove unused statments and codes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,27 +1,13 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
 
 import Theme from "./Theme";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n/config";
 import { Footer } from "./Footer";
 import "./Layout.css";
-import { Seo } from "./Seo";
-import SiteConfigs from "../../SiteConfigs";
 import { Header } from "./Header";
-import tw from "twin.macro";
 
 export const Layout = ({ children, showHeader = true }) => {
-  const data = useStaticQuery(graphql`
-    query HeaderQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <I18nextProvider i18n={i18n}>
       <Theme>
