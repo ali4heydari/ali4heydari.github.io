fa0f4d8581d3d6e62b11e9a0245380dcc68d9ad6
Ali Heydari <ali4heydari@gmail.com>
2020-10-29 20:54:13 +0330

    refactor(pages): 🌌 change container maxWith in 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ import TitleSection from "components/ui/TitleSection";
 const NotFoundPage: React.FC = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Container section>
+    <Container section maxWidth="lg">
       <TitleSection title="404" subtitle="Page not found" center />
       <p className="mt-4 text-center w-full">
         You just hit a route that doesn&#39;t exist... the sadness.
