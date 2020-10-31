57ed21fb41391cdae57c33227c4c8b548334130d
Ali Heydari <ali4heydari@gmail.com>
2020-10-31 19:13:43 +0330

    feat(pages): 🏁 use new skills component in skills page

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,7 +2,7 @@ import Layout from "components/Layout";
 import SEO from "components/SEO";
 import Experience from "components/Experience";
 import Education from "components/Education";
-import Skills from "components/Skills";
+import Skills from "components/SkillsBar";
 
 const ResumePage: React.FC = () => (
   <Layout>
