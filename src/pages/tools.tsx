a97f9d55ff6affb6480b1b900e375db389d20895
Ali Heydari <ali4heydari@gmail.com>
2020-11-01 21:59:07 +0330

    feat(pages): ⚒ add tools page

---
+++ src/pages/tools.tsx
@@ -0,0 +1,38 @@
+import Layout from "components/Layout";
+import SEO from "components/SEO";
+import React from "react";
+import Container from "../components/ui/Container";
+import TitleSection from "../components/ui/TitleSection";
+import FormatHtml from "../components/utils/FormatHtml";
+import { graphql, useStaticQuery } from "gatsby";
+
+const ToolsPage: React.FC = () => {
+  const {
+    markdownRemark: { html, frontmatter },
+  } = useStaticQuery(graphql`
+    query Tools {
+      markdownRemark(frontmatter: { category: { eq: "tools section" } }) {
+        html
+        frontmatter {
+          title
+          subtitle
+        }
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <SEO title={frontmatter.title} />
+      <Container section maxWidth="lg">
+        <TitleSection
+          title={frontmatter.title}
+          subtitle={frontmatter.subtitle}
+        />
+        <FormatHtml content={html} />
+      </Container>
+    </Layout>
+  );
+};
+
+export default ToolsPage;
