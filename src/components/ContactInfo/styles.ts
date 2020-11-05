8cff6058ac874f646c65b674ef1baa16cb2a73ab
Ali Heydari <ali4heydari@gmail.com>
2020-11-05 14:24:33 +0330

    feat(components): 📷 add high resuloution picture to contactInfo

--- src/components/ContactInfo/styles.ts
+++ src/components/ContactInfo/styles.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import tw from "twin.macro";
+import GatsbyImage, { GatsbyImageProps } from "gatsby-image";
 
 export const ContactInfoItem = styled.div`
   ${tw`w-full sm:w-1/3`};
+`;
+
+export const Image = styled(GatsbyImage)<GatsbyImageProps>`
+  ${tw`w-full`}
 `;
