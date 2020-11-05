6a9f243ee9231425f3f34ec72fb35192a5d3acb8
Ali Heydari <ali4heydari@gmail.com>
2020-11-05 13:40:21 +0330

    feat(pages): 📧 add contact me form to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
 import Layout from "components/Layout";
 import SEO from "components/SEO";
 import HeroBanner from "components/HeroBanner";
-import Services from "components/Services";
 import Testimonials from "components/Testimonials";
+import Experience from "../components/Experience";
+import { ContactMe } from "../components/ContactMeForm";
 
 const IndexPage: React.FC = () => {
   return (
     <Layout>
       <SEO title="About Me" />
       <HeroBanner />
-      <Services />
+      <hr />
+      <Experience />
       <hr />
       <Testimonials />
+      <ContactMe />
     </Layout>
   );
 };
