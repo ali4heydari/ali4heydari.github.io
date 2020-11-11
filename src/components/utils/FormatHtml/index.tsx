c9bc8dff7ee2267b7f1d1961be6a01631cc5df49
Ali Heydari <ali4heydari@gmail.com>
2020-11-11 21:54:15 +0330

    refactor(utils): 🎀 update markdown component stayling

--- src/components/utils/FormatHtml/index.tsx
+++ src/components/utils/FormatHtml/index.tsx
@@ -18,16 +18,22 @@ export const MarkDown: React.FC<Props> = ({ content }) => (
   <main
     css={css`
       h1 {
-        ${tw`text-4xl`}
+        ${tw`text-6xl`}
       }
       h2 {
-        ${tw`text-3xl`}
+        ${tw`text-5xl ms-1`}
       }
       h4 {
-        ${tw`text-2xl`}
+        ${tw`text-4xl ms-2`}
       }
       h3 {
-        ${tw`text-xl`}
+        ${tw`text-3xl ms-4`}
+      }
+      h4 {
+        ${tw`text-2xl ms-5`}
+      }
+      h6 {
+        ${tw`text-xl ms-6`}
       }
       ul {
         ${tw`list-disc`}
@@ -39,7 +45,7 @@ export const MarkDown: React.FC<Props> = ({ content }) => (
         ${tw`ms-2`}
       }
       p {
-        ${tw`text-base`}
+        ${tw`text-base ms-6`}
       }
     `}
     dangerouslySetInnerHTML={{
