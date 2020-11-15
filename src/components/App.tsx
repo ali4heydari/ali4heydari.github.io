b5bda57940351db6e8347707d05dd924d2bd2b3e
Ali Heydari <ali4heydari@gmail.com>
2020-11-15 21:54:53 +0330

    fix(components): 🚚 move contexts to app element

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 import { ThemeProvider } from "./ThemeContext";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18n/config";
+import Theme from "./Layout/Theme";
 
 export const App: React.FC = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Theme>
+        <ThemeProvider>{children}</ThemeProvider>
+      </Theme>
+    </I18nextProvider>
+  );
 };
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,9 +9,6 @@ import GlobalStyles from "assets/styles/globalStyles";
 import * as Styled from "./styles";
 import React from "react";
 import SEO from "../SEO";
-import Theme from "./Theme";
-import { I18nextProvider } from "react-i18next";
-import i18n from "../../i18n/config";
 
 interface Props {
   children: React.ReactNode;
@@ -30,26 +27,24 @@ const Layout: React.FC<Props> = ({ children, title = "" }) => {
   `);
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <Theme>
-        <GlobalStyles />
-        <SEO title={title} />
-        <AnimatePresence exitBeforeEnter>
-          <Styled.Layout>
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <motion.div
-              initial={{ y: 30, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ delay: 0.2 }}
-            >
-              {children}
-              <Footer />
-            </motion.div>
-          </Styled.Layout>
-        </AnimatePresence>
-      </Theme>
-    </I18nextProvider>
+    <>
+      <GlobalStyles />
+      <SEO title={title} />
+      <AnimatePresence exitBeforeEnter>
+        <Styled.Layout>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <motion.div
+            initial={{ y: 30, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ delay: 0.2 }}
+          >
+            {children}
+            <Footer />
+          </motion.div>
+        </Styled.Layout>
+      </AnimatePresence>
+    </>
   );
 };
 
