ea40fff36d905dc1236f21383e27e8454c21db4a
Ali Heydari <ali4heydari@gmail.com>
2020-11-20 16:36:07 +0330

    feat(utils): 👩🏻‍💻 update markdown component

--- src/components/utils/MarkDown/index.tsx
+++ src/components/utils/MarkDown/index.tsx
@@ -8,48 +8,7 @@ interface Props {
 }
 
 export const MarkDown: React.FC<Props> = ({ content }) => (
-  <main
-    css={css`
-      h1 {
-        ${tw`text-6xl`}
-      }
-      h2 {
-        ${tw`text-5xl ms-1`}
-      }
-      h4 {
-        ${tw`text-4xl ms-2`}
-      }
-      h3 {
-        ${tw`text-3xl ms-4`}
-      }
-      h4 {
-        ${tw`text-2xl ms-5`}
-      }
-      h6 {
-        ${tw`text-xl ms-6`}
-      }
-      ul {
-        ${tw`list-disc`}
-      }
-      ol {
-        ${tw`list-decimal`}
-      }
-      li {
-        ${tw`ms-2`}
-      }
-      p {
-        ${tw`text-base ms-6`}
-      }
-      h1,
-      h2,
-      h3,
-      h4,
-      h5,
-      h6 > a {
-        ${tw`ms-6`}
-      }
-    `}
-  >
+  <main className="markdown">
     <MDXRenderer children={content} />
   </main>
 );
