d5134c66957b93ba2eaf7450c3d55e5cd7a5d262
Ali Heydari <ali4heydari@gmail.com>
2020-11-22 17:38:42 +0330

    feat(pages): ✒ add disqus to tools page

--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -4,10 +4,19 @@ import Container from "../components/ui/Container";
 import TitleSection from "../components/ui/TitleSection";
 import { graphql, useStaticQuery } from "gatsby";
 import { MarkDown } from "../components/utils/MarkDown";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Disqus } from "gatsby-plugin-disqus";
 
+const DisqusComponent = styled(Disqus)`
+  ${tw`w-full mt-3`}
+`;
 const ToolsPage: React.FC = () => {
   const {
     mdx: { body, frontmatter },
+    site: {
+      siteMetadata: { siteUrl },
+    },
   } = useStaticQuery(graphql`
     query Tools {
       mdx(frontmatter: { category: { eq: "tools section" } }) {
@@ -15,6 +24,11 @@ const ToolsPage: React.FC = () => {
         frontmatter {
           title
           subtitle
+        }
+      }
+      site {
+        siteMetadata {
+          siteUrl
         }
       }
     }
@@ -28,6 +42,13 @@ const ToolsPage: React.FC = () => {
           subtitle={frontmatter.subtitle}
         />
         <MarkDown content={body} />
+        <DisqusComponent
+          config={{
+            url: `${siteUrl}/tools`,
+            identifier: "tools",
+            title: frontmatter.title,
+          }}
+        />
       </Container>
     </Layout>
   );
