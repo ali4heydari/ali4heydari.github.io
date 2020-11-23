7f29f3243780474eb102c0213fe04d374cb5ba66
Ali Heydari <ali4heydari@gmail.com>
2020-11-23 13:33:49 +0330

    feat(pages): 📅 add calendly to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,11 +1,14 @@
 import Layout from "components/Layout";
 import ContactInfo from "components/ContactInfo";
 import React from "react";
+import { InlineWidget } from "react-calendly";
+import SiteConfigs from "../../SiteConfigs";
 
 const ContactPage: React.FC = () => {
   return (
     <Layout title="Contact">
       <ContactInfo />
+      <InlineWidget url={`https://calendly.com/${SiteConfigs.CALENDLY_ID}`} />
     </Layout>
   );
 };
