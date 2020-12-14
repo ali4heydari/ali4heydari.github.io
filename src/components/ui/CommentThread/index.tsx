71b1c3f2b7f3538019ad646bf21650c274caa546
Ali Heydari <ali4heydari@gmail.com>
2020-12-14 10:23:19 +0330

    fix(ui): 🎭 add theme prop to CommentThread to rerender on theme change

--- src/components/ui/CommentThread/index.tsx
+++ src/components/ui/CommentThread/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import * as Styled from "./styles";
+import { ContextType, ThemeContext } from "../../ThemeContext";
+import { useTranslation } from "react-i18next";
 
-export const CommentThread: React.FC<Styled.DisqusProps> = ({
-  config,
-  theme,
-}: Styled.DisqusProps) => {
-  return <Styled.DisqusThread config={config} theme={theme} />;
+export const CommentThread: React.FC<Styled.DisqusProps> = (
+  props: Styled.DisqusProps
+) => {
+  const { colorMode } = React.useContext<ContextType>(ThemeContext);
+  const { i18n } = useTranslation();
+
+  const {
+    config: { language = i18n.language, ...rest },
+    theme = colorMode,
+  } = props;
+
+  return (
+    <Styled.DisqusThread
+      config={{
+        language,
+        ...rest,
+      }}
+      theme={theme}
+    />
+  );
 };
