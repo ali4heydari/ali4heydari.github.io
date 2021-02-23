43226d38d67a33eddfc957a9b8de908c1a63bba8
Ali Heydari <ali4heydari@gmail.com>
2021-02-23 09:26:39 +0330

    feat(components): 🦶 add spotify to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,33 +6,39 @@ import MainNav from "./MainNav";
 import tw from "twin.macro";
 import styled from "styled-components";
 import { socialMedias } from "../../socialMedias";
+import { NowPlaying } from "../Spotify";
 
-const Footer: React.FC = () => (
-  <Styled.Footer>
-    <Container maxWidth="lg">
-      <Styled.Links>
-        {socialMedias.map((media) => {
-          const Icon = styled(media.icon)`
-            ${tw`w-6 h-6 m-1`}
-          `;
+const Footer: React.FC = () => {
+  return (
+    <Styled.Footer>
+      <Container maxWidth="lg">
+        <div css={tw`flex flex-wrap items-center justify-center w-full`}>
+          <NowPlaying />
+        </div>
+        <Styled.Links>
+          {socialMedias.map((media) => {
+            const Icon = styled(media.icon)`
+              ${tw`w-6 h-6 m-1`}
+            `;
 
-          return (
-            <Styled.Link
-              key={media.link}
-              href={media.link}
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-              aria-label={`link to authors'${media.name} account`}
-            >
-              <Icon />
-            </Styled.Link>
-          );
-        })}
-      </Styled.Links>
-      <Styled.Separator center />
-      <MainNav />
-    </Container>
-  </Styled.Footer>
-);
+            return (
+              <Styled.Link
+                key={media.link}
+                href={media.link}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                aria-label={`link to authors'${media.name} account`}
+              >
+                <Icon />
+              </Styled.Link>
+            );
+          })}
+        </Styled.Links>
+        <Styled.Separator center />
+        <MainNav />
+      </Container>
+    </Styled.Footer>
+  );
+};
 
 export default Footer;
