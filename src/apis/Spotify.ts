cabfb62fe38f01dc534b7169d141f9e4fb47289b
Ali Heydari <ali4heydari@gmail.com>
2021-04-12 10:20:26 +0430

    fix(apis): 🎶 fix spotify now playing
    fix #60

--- src/apis/Spotify.ts
+++ src/apis/Spotify.ts
@@ -1,47 +1,17 @@
-import * as querystring from "querystring";
-import { Buffer } from "buffer";
+const BASE_URL = `https://api-ali4heydari.herokuapp.com`;
 
-const client_id = process.env.GATSBY_SPOTIFY_CLIENT_ID;
-const client_secret = process.env.GATSBY_SPOTIFY_CLIENT_SECRET;
-const refresh_token = process.env.GATSBY_SPOTIFY_REFRESH_TOKEN;
-
-const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
-const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
-const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
-
-export const getAccessToken = async () => {
-  const response = await fetch(TOKEN_ENDPOINT, {
-    method: "POST",
-    headers: {
-      Authorization: `Basic ${basic}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: querystring.stringify({
-      grant_type: "refresh_token",
-      refresh_token,
-    }),
-  });
-
-  return response.json();
-};
+const NOW_PLAYING_ENDPOINT = `${BASE_URL}/musics/getNowPlaying`;
+const TOP_TRACKS_ENDPOINT = `${BASE_URL}/musics/getTopTracks`;
+const TOP_ARTISTS_ENDPOINT = `${BASE_URL}/musics/getTopArtists`;
 
 export const getNowPlaying = async () => {
-  const { access_token } = await getAccessToken();
-
-  return fetch(NOW_PLAYING_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
+  return fetch(NOW_PLAYING_ENDPOINT);
 };
 
 export const getTopTracks = async () => {
-  const { access_token } = await getAccessToken();
+  return fetch(TOP_TRACKS_ENDPOINT);
+};
 
-  return fetch(TOP_TRACKS_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
+export const getTopArtists = async () => {
+  return fetch(TOP_ARTISTS_ENDPOINT);
 };
