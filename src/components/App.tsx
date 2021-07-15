7f351b033119f8e8fbde9b1a4ba68cd9c0c65364
Ali Heydari <ali4heydari@gmail.com>
2021-07-15 23:57:59 +0200

    feat(global): 🌓 improve dark mode logic and codes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { ThemeProvider } from "./ThemeContext";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n/config";
 import Theme from "./Layout/Theme";
@@ -8,9 +7,7 @@ import Theme from "./Layout/Theme";
 export const App: React.FC = ({ children }) => {
   return (
     <I18nextProvider i18n={i18n}>
-      <Theme>
-        <ThemeProvider>{children}</ThemeProvider>
-      </Theme>
+      <Theme>{children}</Theme>
     </I18nextProvider>
   );
 };
