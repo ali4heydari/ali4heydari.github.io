94a9b74ed4dc873eab1043bf33a41f8aa424d388
Ali Heydari <ali4heydari@gmail.com>
2022-01-18 22:37:21 +0330

    wip fix edu compelete

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,26 +1,27 @@
 import Layout from "src/components/Layout";
 import SEO from "src/components/SEO";
 import TimeLine from "src/components/Experience";
-import Education from "src/components/Education";
+import Education from "src/components/Educations";
 import React from "react";
 import { useMDXComponent } from "next-contentlayer/hooks";
-import { allExperiences } from ".contentlayer/data";
+import { allExperiences, allEducation } from ".contentlayer/data";
 import Timeline from "../src/components/ui/Timeline";
 import TitleSection from "../src/components/ui/TitleSection";
 import Container from "src/components/ui/Container";
+import Educations from "src/components/Educations";
 
-const ResumePage: React.FC<any> = ({ allExperiences }) => {
+const ResumePage: React.FC<any> = ({ allExperiences, allEducation }) => {
   return (
     <Layout title="Resume">
       <TimeLine allExperiences={allExperiences} />
       <hr />
-      {/*<Education />*/}
+      <Educations allEducation={allEducation} />
     </Layout>
   );
 };
 
 export async function getStaticProps({ params }) {
-  return { props: { allExperiences } };
+  return { props: { allExperiences, allEducation } };
 }
 
 export default ResumePage;
