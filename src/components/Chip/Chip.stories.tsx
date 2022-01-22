55be251076133906dcdf84cc56a6d6fb708a3928
Ali Heydari <ali4heydari@gmail.com>
2022-01-23 00:27:00 +0330

    refactor(components): 🧹 refactor chip component

--- src/components/Chip/Chip.stories.tsx
+++ src/components/Chip/Chip.stories.tsx
@@ -1,18 +1,58 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
+import * as tailwindColors from "tailwindcss/colors";
 import Chip from "../Chip";
 
 export default {
   title: "Molecules/Chip",
   component: Chip,
+  argTypes: {
+    children: {
+      control: {
+        type: "text",
+      },
+    },
+    type: {
+      control: {
+        type: "radio",
+        options: [
+          "default",
+          "primary",
+          "secondary",
+          "success",
+          "danger",
+          "warning",
+          "info",
+        ],
+      },
+    },
+  },
+  parameters: {
+    background: {
+      default: "light",
+      values: [
+        {
+          name: "light",
+          value: tailwindColors.white,
+        },
+        {
+          name: "dark",
+          value: tailwindColors.gray["800"],
+        },
+      ],
+    },
+  },
 } as ComponentMeta<typeof Chip>;
 
-const Template: ComponentStory<typeof Chip> = (args) => <Chip {...args} />;
+const Template: ComponentStory<typeof Chip> = (args) => (
+  <div className="max-w-lg">
+    <Chip {...args} />
+  </div>
+);
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-  href: "https://example.com",
   children: "this in children",
+  type: "primary",
 };
--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import styles from "./Chip.module.css";
+import classNames from "classnames";
 
 interface ChipProps {
-  href?: string;
   children?: React.ReactNode;
+  type?:
+    | "default"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info";
 }
 
-const Chip: React.FC<ChipProps> = ({ children, href }: ChipProps) => {
+const Chip: React.FC<ChipProps> = ({
+  children,
+  type = "default",
+}: ChipProps) => {
   return (
-    <div className={styles.root}>
-      <a className={styles.content} href={href}>
-        {children}
-      </a>
-    </div>
+    <div className={classNames(styles.root, styles[type])}>{children}</div>
   );
 };
 
