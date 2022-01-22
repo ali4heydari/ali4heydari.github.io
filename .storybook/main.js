bd034ee1be1a02d3f1d2b5e0383dd3792a0933a7
Ali Heydari <ali4heydari@gmail.com>
2022-01-22 10:47:54 +0330

    feat(*): 📚 add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,91 @@
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+    "storybook-css-modules-preset",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: async (config) => {
+    /**
+     * Add support for alias-imports
+     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
+     */
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      "@": [path.resolve(__dirname, "../src/"), path.resolve(__dirname, "../")],
+    };
+
+    /**
+     * Fixes font import with /
+     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
+     */
+    config.resolve.roots = [
+      path.resolve(__dirname, "../public"),
+      "node_modules",
+    ];
+
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin(),
+    ];
+
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack"),
+    });
+
+    // https://github.com/tailwindlabs/tailwindcss/issues/3258
+    config.module.rules.forEach((loaders) => {
+      if (Array.isArray(loaders.use)) {
+        loaders.use.forEach((l) => {
+          if (
+            typeof l !== "string" &&
+            typeof l.loader === "string" &&
+            /(?<!post)css-loader/.test(l.loader)
+          ) {
+            if (!l.options.modules) return;
+            const { getLocalIdent, ...others } = l.options.modules;
+            l.options = {
+              ...l.options,
+              modules: {
+                ...others,
+                getLocalIdent: (ctx, localIdentName, localName) => {
+                  if (localName === "dark") return localName;
+                  return null;
+                },
+              },
+            };
+          }
+        });
+      }
+    });
+
+    return config;
+  },
+  // https://github.com/storybookjs/storybook/issues/15336#issuecomment-888528747
+  typescript: { reactDocgen: false },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import "../src/styles/global.css";
+import * as NextImage from "next/image";
+
+// https://storybook.js.org/blog/get-started-with-storybook-and-next-js/
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
