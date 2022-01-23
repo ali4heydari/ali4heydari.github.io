ee2bff12fddf6e80c4d60abf2fb945c6d3377b82
Ali Heydari <ali4heydari@gmail.com>
2022-01-23 23:07:26 +0330

    feat(layouts): 🖼 add layout component

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,27 @@
+import Header from "src/components/Header";
+import Footer from "src/components/Footer";
+import type { ReactNode } from "react";
+import classNames from "classnames";
+
+type Props = {
+  children: ReactNode;
+  container?: boolean;
+};
+
+const MainLayout = ({ children, container }: Props): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <main
+        className={classNames({
+          "container px-8": container,
+        })}
+      >
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
