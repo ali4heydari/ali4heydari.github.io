c00e5612a12c896cfd9817b9d24d7605533429e9
Ali Heydari <ali4heydari@gmail.com>
2022-01-23 17:36:42 +0330

    fix(.storybook): 🔧 expose publick folder for stroybook in right way

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  /** Expose public folder to storybook as static */
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
