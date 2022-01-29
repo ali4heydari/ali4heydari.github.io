f014441f3b1dafe1bd0d2a2f17a0e8cf539ee527
Ali Heydari <ali4heydari@gmail.com>
2022-01-29 22:00:32 +0330

    refactor(features): 🧹 fix exprince feature title and reverse events

--- src/features/Experiences/index.tsx
+++ src/features/Experiences/index.tsx
@@ -1,34 +1,37 @@
-import Event from "src/components/Timeline/components/TimelineItem";
 import Container from "src/components/Container";
 import TitleSection from "src/components/TitleSection";
 import React from "react";
 import Timeline from "../../components/Timeline";
-import { EventProps } from "../../components/Timeline/@types";
 import MdxRenderer from "../../components/MdxRenderer";
 import type { Experience } from ".contentlayer/types";
 
 const Educations: React.FC<{
   allExperiences: Experience[];
 }> = ({ allExperiences }) => {
-  const events = allExperiences.map(
-    ({
-      position: subtitle,
-      company: title,
-      startDate,
-      endDate,
-      body: { code },
-    }) => ({
-      title,
-      subtitle,
-      startDate,
-      endDate,
-      children: <MdxRenderer code={code} />,
-    })
-  );
+  const events = allExperiences
+    .map(
+      ({
+        position: subtitle,
+        company: title,
+        startDate,
+        endDate,
+        body: { code },
+      }) => ({
+        title,
+        subtitle,
+        startDate,
+        endDate,
+        children: <MdxRenderer code={code} />,
+      })
+    )
+    .reverse();
 
   return (
     <Container section maxWidth="lg">
-      <TitleSection title={"Education"} subtitle={"My Qualification"} />
+      <TitleSection
+        title={"MY EXPERIENCE"}
+        subtitle={"Companies and university"}
+      />
       <Timeline events={events} />
     </Container>
   );
