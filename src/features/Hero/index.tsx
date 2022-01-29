cf8880b3cea94fb16b0fa27e19d66ebd85160872
Ali Heydari <ali4heydari@gmail.com>
2022-01-29 22:01:41 +0330

    fix(features): 🔧 make hero center

--- src/features/Hero/index.tsx
+++ src/features/Hero/index.tsx
@@ -4,7 +4,7 @@ import Banner from "./components/Banner";
 
 const HeroBanner: React.FC = () => {
   return (
-    <div className="flex flex-row-reverse items-center">
+    <div className="mx-auto flex flex-row-reverse items-center justify-center">
       <Banner />
       <Introduction />
     </div>
