aadfa09abae6a270061db14590f0f28741b718bb
Ali Heydari <ali4heydari@gmail.com>
2022-03-19 23:02:32 +0330

    refactor(components): ✒ change props of CommentTheread component

--- src/components/CommentThread/index.tsx
+++ src/components/CommentThread/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { DiscussionEmbed } from "disqus-react";
+import { DiscussionEmbed, CommentEmbed } from "disqus-react";
 import siteConfig from "site.config";
+import { useRouter } from "next/router";
 
 const CommentThread: React.FC<any> = (props) => {
+  const router = useRouter();
   return (
     <DiscussionEmbed
       shortname={siteConfig.DISCUSS_SHORTNAME}
       config={{
         language: "en-US",
+        url: router.asPath,
+        // TODO: add title
+        // title: "test",
+        identifier: router.asPath,
       }}
     />
   );
