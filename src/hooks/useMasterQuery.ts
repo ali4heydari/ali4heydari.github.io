0ddbeb93666c42d4a0e746e0bc080a91cb6ae09f
Ali Heydari <ali4heydari@gmail.com>
2022-03-19 22:51:32 +0330

    feat(hooks): ⚓ add useMasterQuery hook

---
+++ src/hooks/useMasterQuery.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "react-query";
+import type {
+  QueryFunction,
+  QueryKey,
+  UseQueryOptions,
+  UseQueryResult,
+} from "react-query";
+
+const useMasterQuery = <
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey
+>(
+  queryKey: TQueryKey,
+  queryFn: QueryFunction<TQueryFnData, TQueryKey>,
+  options?: Omit<
+    UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
+    "queryKey" | "queryFn"
+  >
+): UseQueryResult<TData, TError> => {
+  const globalOption = {
+    // global options for all queries
+    ...options,
+  };
+
+  return useQuery(queryKey, queryFn, globalOption);
+};
+
+export default useMasterQuery;
