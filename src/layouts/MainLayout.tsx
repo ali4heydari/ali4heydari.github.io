61c7e9da1eb32a14bc8fd3e5e873dccc3bdc85be
Ali Heydari <ali4heydari@gmail.com>
2022-03-28 14:44:26 +0430

    fix(layouts): 📸 avoid flash on first render

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,13 +2,20 @@ import Header from "src/components/Header";
 import Footer from "src/components/Footer";
 import type { ReactNode } from "react";
 import classNames from "classnames";
+import { useEffect, useState } from "react";
 
 type Props = {
   children: ReactNode;
   container?: boolean;
 };
 
-const MainLayout = ({ children, container }: Props): JSX.Element => {
+const MainLayout = ({ children, container }: Props): JSX.Element | null => {
+  const [mounted, setMounted] = useState(false);
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
     <>
       <Header />
