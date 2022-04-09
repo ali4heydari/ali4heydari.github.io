57c247e8c8b760fc6e7e33f2531ea336dddbd697
Ali Heydari <ali4heydari@gmail.com>
2022-04-09 23:16:57 +0430

    refactor(@types): remove unused declarations

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,5 +1,4 @@
 import React from "react";
-import { CSSProp } from "styled-components";
 
 declare module "*.jpg";
 declare module "*.png";
@@ -7,11 +6,6 @@ declare module "*.jpg";
 declare module "*.json";
 
 declare module "react" {
-  interface Attributes {
-    css?: CSSProp;
-    // https://github.com/emotion-js/emotion/issues/1137#issuecomment-615571356
-    as?: React.ElementType | keyof JSX.IntrinsicElements;
-  }
 
   declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
