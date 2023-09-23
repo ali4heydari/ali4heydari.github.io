654325d99bb2125ad268fa4898b8d94e1753f065
Ali Heydari <ali4heydari@gmail.com>
2023-09-24 02:37:04 +0330

    feat: add skills to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Hero from "../features/Hero";
 import Experiences from "../features/Experiences";
 import { NextPage } from "next";
 import { allExperiences } from "../../.contentlayer/generated";
+import { Skills } from "../features/Skills";
 
 const Home: NextPage = () => {
   return (
     <>
       <Hero />
       <Experiences allExperiences={allExperiences} />
+      <Skills />
     </>
   );
 };
