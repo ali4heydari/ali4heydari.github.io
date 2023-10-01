460dab66dec34fcef4f7aee516e30b0ca92f2fa0
Ali Heydari <ali4heydari@gmail.com>
2023-10-01 15:16:09 +0330

    refactor: utils

--- src/utils/functions/enum-to-value-array/enumToValueArray.ts
+++
@@ -1,7 +0,0 @@
-export function enumToValueArray($enum: any): string[] {
-  const result: string[] = [];
-  for (let i in $enum) {
-    result.push($enum[i]);
-  }
-  return result;
-}
--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -1,1 +1,11 @@
-export * from "./enum-to-value-array";
+export function enumToValueArray($enum: any): string[] {
+  const result: string[] = [];
+  for (let i in $enum) {
+    result.push($enum[i]);
+  }
+  return result;
+}
+
+export const isValidDate = (date: string | number) =>
+  Object.prototype.toString.call(date) === "[object Date]" &&
+  !Number.isNaN(date);
