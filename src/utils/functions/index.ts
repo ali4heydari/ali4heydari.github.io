eae818097cbd51b26219b154e194241059b96ba9
Ali Heydari <ali4heydari@gmail.com>
2023-10-12 23:16:28 +0330

    feat: add getJson util func

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -9,3 +9,22 @@ export function enumToValueArray($enum: any): string[] {
 export const isValidDate = (date: string | number) =>
   Object.prototype.toString.call(date) === "[object Date]" &&
   !Number.isNaN(date);
+
+type WithStatus<TResponseBody> = {
+  data: TResponseBody;
+  status: number;
+};
+
+export const getJson = async <TResponseBody = any>(
+  url: string,
+  options?: RequestInit,
+): Promise<WithStatus<TResponseBody>> =>
+  fetch(url, options).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json().then<WithStatus<TResponseBody>>((data) => ({
+      data,
+      status: response.status,
+    }));
+  });
