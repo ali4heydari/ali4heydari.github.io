02158b79907ac92eb6d060cc5a81e54c761271d0
Ali Heydari <ali4heydari@gmail.com>
2023-10-14 23:35:40 +0330

    chore: rewrite introdcution text

--- src/features/Introduction/index.tsx
+++ src/features/Introduction/index.tsx
@@ -15,28 +15,40 @@ const About: React.FC<React.PropsWithChildren<unknown>> = () => {
         width={900}
       />
       <p className="text-lg leading-8 text-gray-600 dark:text-gray-300 pb-20">
-        As a digital artisan, I proudly wield the title of a front-end
-        developer, complemented by a touch of back-end proficiency. My
-        technological odyssey has led me to embrace the finesse of automation,
-        the allure of Docker, the orchestration of DevOps, the intricacies of
-        networking, and the symphony of CI/CD. With academic distinction, I hold
-        a bachelor&#39;s degree in computer engineering from Iran University of
-        Science and Technology, a testament to my fortified foundation in this
-        ever-evolving field. During moments of respite, I immerse myself in the
-        pursuit of knowledge, exploring new technologies and indulging in the
-        immersive narratives of video games. Beyond the realm of code, I find
-        joy in the culinary arts, aspiring to master the craft professionally.
-        Additionally, the delicate artistry of flower design has captivated my
-        interest, as I have participated in a workshop to refine my skills. In
-        the realm of leisure, I find solace in the dulcet tones of music and the
-        captivating narratives of podcasts. These auditory delights transport me
-        to realms unknown, expanding the horizons of my consciousness. Since the
-        tender age of 18, I have embraced independence, residing in a city
-        separate from my beloved family. This journey has instilled within me
-        self-reliance and resilience, as I navigate life&#39;s currents with
-        unwavering determination. Welcome to my digital realm, where technology
-        and creativity converge, and where the symphony of code and design
-        harmonize to create a captivating experience.
+        Greetings, fellow wanderers of the digital realm! I am a front-end
+        developer extraordinaire, with a sprinkle of back-end wizardry up my
+        sleeve. I have traversed the realms of automation, danced with the
+        mystical Docker, harmonized with the enchanting melodies of DevOps, and
+        woven intricate networks with a touch of CI/CD magic. My journey began
+        at the illustrious Iran University of Science and Technology, where I
+        delved into the captivating world of computer engineering, earning my
+        bachelor&#39;s degree.
+        <br />
+        When I&#39;m not immersed in the captivating world of coding, you can
+        find me embarking on thrilling quests to unravel the mysteries of new
+        technologies or engaging in epic battles within the realms of video
+        games. But my adventures don&#39;t end there! I have a burning passion
+        for the culinary arts, seeking to master the art of cooking with finesse
+        and flair. From whipping up delectable dishes to exploring the vast
+        universe of flavors, I&#39;m always hungry for new culinary adventures.
+        <br />
+        In the realm of aesthetics, I have discovered a hidden talent for flower
+        design. I recently participated in a workshop that revealed the secrets
+        of creating stunning floral arrangements. It&#39;s a delightful way for
+        me to express my creativity and bring a touch of natural beauty into the
+        world.
+        <br />
+        When I&#39;m not busy crafting lines of code or experimenting in the
+        kitchen, you&#39;ll often find me lost in the captivating melodies of
+        music or engrossed in the captivating narratives of podcasts. And
+        let&#39;s not forget my fascination with the cosmos! I find solace in
+        gazing at the twinkling stars, exploring the wonders of the universe
+        through the lens of astronomy.
+        <br />
+        So, come join me on this whimsical journey as I weave together my love
+        for technology, creativity, and the boundless wonders of the universe.
+        Together, let&#39;s create a digital tapestry that reflects the vibrant
+        colors of friendship and the joy of exploration!
       </p>
     </section>
   );
