123e0b96d34934f1b16a3e7b26b13f8c45db90e2
Ali Heydari <ali4heydari@gmail.com>
2023-10-15 00:18:01 +0330

    fix: close mobile menu on click links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -70,6 +70,7 @@ const Header = () => {
                   <Link
                     key={item.name}
                     href={item.href}
+                    onClick={() => setMobileMenuOpen(false)}
                     className="-mx-3 block  rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-700 dark:hover:bg-gray-800"
                   >
                     {item.name}
