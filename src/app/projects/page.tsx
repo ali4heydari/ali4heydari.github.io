cb1610718243a131186d7b3e767b31f4f69995ae
Ali Heydari <ali4heydari@gmail.com>
2023-10-17 22:24:56 +0330

    refactor: projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,11 +5,12 @@ import Link from "next/link";
 import TitleSection from "../../components/TitleSection";
 import { buildOgImageUrl } from "../../utils/opengraph";
 import { getStaticMetadata } from "../../utils/metadata";
+import Image from "next/image";
 
 export const metadata = getStaticMetadata({
   title: "Projects – Ali Heydari",
   description:
-    "Projects by Ali Heydari. Get to know the projects I've worked on.",
+    "Projects by Ali Heydari. Get to know the projects I&#39;ve worked on.",
   exactUrl: "https://ali4heydari.tech/projects",
   keywords: [
     "tech",
@@ -28,37 +29,54 @@ const ProjectsPage: NextPage = () => {
   return (
     <section>
       <TitleSection center>Projects</TitleSection>
-      <div className="grid gap-20 sm:grid-cols-2 my-10">
-        {allProjects.map(({ title, summary, _id, tags, slug, cover }) => (
-          <article key={_id} className="w-full flex justify-between">
-            <div className="w-2/3 flex flex-col justify-between">
-              <div>
-                <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                  <Link href={`/projects/${slug}`}>{title}</Link>
-                </h2>
+      <p className="text-xl text-gray-600 dark:text-gray-400 mb-4">
+        Here is a list of projects that I&#39;ve worked on. I&#39;ve worked on
+        many more projects, but I can&#39;t share them here because of the NDA.
+      </p>
+      <div className="grid gap-20 grid-cols-1 lg:grid-cols-2 my-10">
+        {allProjects.map((project) => (
+          <Link
+            key={project._id}
+            href={`/projects/${project.slug}`}
+            className="border-2 border-gray-200 dark:border-gray-700 rounded-lg p-5 hover:border-gray-400 dark:hover:border-gray-500 transition-colors duration-200"
+          >
+            <article className="w-full flex justify-between">
+              <div className="w-2/3 flex flex-col justify-between">
+                <div>
+                  <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                    {project.title}
+                  </h2>
+                  <p className="flex flex-wrap font-light justify-start gap-1 text-xs text-gray-500 dark:text-gray-400">
+                    <time>
+                      🗓️ {project.startDate} - {project.endDate ?? "Present"}
+                    </time>
+                    <span>⏳ {project.readingTime.text}</span>
+                  </p>
+                </div>
                 <p className="mb-4 text-gray-500 dark:text-gray-400">
-                  {summary}
+                  {project.summary}
                 </p>
+                <div className="flex flex-wrap pb-3">
+                  {project.tags.slice(0, 4).map((tag) => (
+                    <span
+                      key={tag}
+                      className="m-0.5 bg-gray-100 text-gray-800 text-xs font-light inline-flex items-center px-1.5 py-0.5 rounded mr-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
               </div>
-              <div className="flex flex-wrap pb-3">
-                {tags.slice(0, 4).map((tag) => (
-                  <span
-                    key={tag}
-                    className="m-0.5 bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500"
-                  >
-                    {tag}
-                  </span>
-                ))}
-              </div>
-            </div>
-            <Link href={`/projects/${slug}`} className="">
-              <img
-                src={cover}
-                className="mb-5 rounded-lg object-cover sm:h-64"
-                alt="Image 1"
+
+              <Image
+                src={project.cover}
+                className="mb-5 rounded-lg object-cover h-64 aspect-[2/3]"
+                alt={project.title}
+                width={600}
+                height={900}
               />
-            </Link>
-          </article>
+            </article>
+          </Link>
         ))}
       </div>
     </section>
