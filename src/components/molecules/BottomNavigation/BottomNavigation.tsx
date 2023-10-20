c804915df54cb74a424e70fe4649961b4753153e
Ali Heydari <ali4heydari@gmail.com>
2023-10-21 02:16:16 +0330

    refactor: replace blue colors with indigo

--- src/components/molecules/BottomNavigation/BottomNavigation.tsx
+++ src/components/molecules/BottomNavigation/BottomNavigation.tsx
@@ -7,7 +7,7 @@ import { usePathname } from "next/navigation";
 const BottomNavigation = () => {
   const pathname = usePathname();
   return (
-    <div className="sticky bottom-0 left-0 z-50 w-full h-16 border-t border-gray-200 bg-gray-100 dark:bg-gray-800 dark:shadow-blue-400 dark:border-gray-600 lg:hidden">
+    <div className="sticky bottom-0 left-0 z-50 w-full h-16 border-t border-gray-200 bg-gray-100 dark:bg-gray-800 dark:shadow-indigo-400 dark:border-gray-600 lg:hidden">
       <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
         {navigation.primary.map((item) => (
           <Link
@@ -22,13 +22,15 @@ const BottomNavigation = () => {
             <item.svgIcon
               className={classNames(
                 "w-5 h-5 mb-2 text-gray-500 dark:text-gray-400",
-                pathname === item.href && "text-blue-600 dark:text-blue-500",
+                pathname === item.href &&
+                  "text-indigo-600 dark:text-indigo-500",
               )}
             />
             <span
               className={classNames(
                 "text-sm text-gray-500 dark:text-gray-400",
-                pathname === item.href && "text-blue-600 dark:text-blue-500",
+                pathname === item.href &&
+                  "text-indigo-600 dark:text-indigo-500",
               )}
             >
               {item.name}
