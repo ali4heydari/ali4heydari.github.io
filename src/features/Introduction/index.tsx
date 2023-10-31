5ba1ae59d847c05ebc267c8c643d285c47d2b887
Ali Heydari <ali4heydari@gmail.com>
2023-10-31 21:46:28 +0330

    docs: update intro

--- src/features/Introduction/index.tsx
+++ src/features/Introduction/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import SiteConfigs from "site.config";
 import meInFrontOfALaptop from "src/assets/images/me/in-front-of-a-laptop.jpg";
+import StyledLink from "src/components/atoms/Link";
 import TitleSection from "src/components/atoms/TitleSection";
 import Image from "next/image";
+import Link from "next/link";
 
 const About: React.FC<React.PropsWithChildren<unknown>> = () => {
   return (
@@ -17,40 +19,27 @@ const About: React.FC<React.PropsWithChildren<unknown>> = () => {
         width={900}
       />
       <p className="pb-20 text-lg leading-8 text-gray-600 dark:text-gray-300">
-        Greetings, fellow wanderers of the digital realm! I am a front-end
-        developer extraordinaire, with a sprinkle of back-end wizardry up my
-        sleeve. I have traversed the realms of automation, danced with the
-        mystical Docker, harmonized with the enchanting melodies of DevOps, and
-        woven intricate networks with a touch of CI/CD magic. My journey began
-        at the illustrious Iran University of Science and Technology, where I
-        delved into the captivating world of computer engineering, earning my
-        bachelor&#39;s degree.
+        Greetings, I&#39;m Ali Heydari. I&#39;m a front-end developer with a
+        some experience in back-end development and DevOps. I build, maintain,
+        and design websites and web applications. I&#39;ve been coding for over
+        5 years and I&#39;m always looking to learn new things.
         <br />
-        When I&#39;m not immersed in the captivating world of coding, you can
-        find me embarking on thrilling quests to unravel the mysteries of new
-        technologies or engaging in epic battles within the realms of video
-        games. But my adventures don&#39;t end there! I have a burning passion
-        for the culinary arts, seeking to master the art of cooking with finesse
-        and flair. From whipping up delectable dishes to exploring the vast
-        universe of flavors, I&#39;m always hungry for new culinary adventures.
+        I entered Chemical Engineering at the Sharif University of Technology on
+        2015. But I was always interested in coding and web development. So I
+        changed my major to Computer Engineering at the Iran University of
+        Science and Technology on 2016. I graduated on 2020 and started working
+        as a front-end developer.
         <br />
-        In the realm of aesthetics, I have discovered a hidden talent for flower
-        design. I recently participated in a workshop that revealed the secrets
-        of creating stunning floral arrangements. It&#39;s a delightful way for
-        me to express my creativity and bring a touch of natural beauty into the
-        world.
-        <br />
-        When I&#39;m not busy crafting lines of code or experimenting in the
-        kitchen, you&#39;ll often find me lost in the captivating melodies of
-        music or engrossed in the captivating narratives of podcasts. And
-        let&#39;s not forget my fascination with the cosmos! I find solace in
-        gazing at the twinkling stars, exploring the wonders of the universe
-        through the lens of astronomy.
-        <br />
-        So, come join me on this whimsical journey as I weave together my love
-        for technology, creativity, and the boundless wonders of the universe.
-        Together, let&#39;s create a digital tapestry that reflects the vibrant
-        colors of friendship and the joy of exploration!
+        Besides coding, I&#39; also enjoy cooking and baking. I&#39;m not really
+        good at it, but I&#39; love to experiment with new recipes. Also I like
+        to play video games and watch movies and TV shows. I&#39;m almost always
+        listening to music. When I&#39;m code, drive, or the house chores. Also
+        I like to listen to podcasts. you can find my favorite musics and
+        podcasts on
+        <Link className="px-1 text-indigo-400 underline" href="/music-taste">
+          Music taste
+        </Link>
+        page.
       </p>
     </section>
   );
