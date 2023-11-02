3fbaa481f41966eb768a2177964ce6557bcb486c
Ali Heydari <ali4heydari@gmail.com>
2023-11-02 23:46:22 +0330

    refactor: remove unused files

--- src/utils/enums/Languages.ts
+++
@@ -1,5 +0,0 @@
-export enum Languages {
-  CI_MODE = "cimode",
-  ENGLISH = "en",
-  PERSIAN = "fa",
-}
--- src/utils/enums/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Languages";
-export * from "./Namespaces";
-export * from "./StringKeys";
