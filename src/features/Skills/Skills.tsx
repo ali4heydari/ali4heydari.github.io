875d7388185fe43f9ba9bfe0f57875cd0efb6415
Ali Heydari <ali4heydari@gmail.com>
2023-11-15 00:22:33 +0330

    feat: add href to skills

--- src/features/Skills/Skills.tsx
+++ src/features/Skills/Skills.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import classNames from "classnames";
 import TitleSection from "src/components/atoms/TitleSection";
 import { skills } from "./constants";
+import Link from "next/link";
 
 export const Skills = ({ rootClassName }: { rootClassName?: string }) => {
   return (
@@ -8,13 +10,19 @@ export const Skills = ({ rootClassName }: { rootClassName?: string }) => {
       <TitleSection center>Skills</TitleSection>
       <div className="flex flex-wrap items-center justify-center py-5 text-sm">
         {skills.map((s) => (
-          <span
+          <Link
+            href={s.href}
             key={s.title}
-            className="m-2 flex items-center rounded-md border border-gray-300 px-4 py-1 dark:border-gray-700"
+            title={`Learn more about ${s.title}`}
+            target="_blank"
+            className={classNames(
+              "m-2 flex items-center rounded-md border border-gray-300 px-4 py-1 transition-colors duration-200",
+              "hover:border-gray-400 hover:bg-gray-50 hover:shadow-sm dark:border-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-800 dark:hover:shadow-md",
+            )}
           >
             {s.icon}
             <span className="ml-4">{s.title}</span>
-          </span>
+          </Link>
         ))}
       </div>
     </section>
--- src/features/Skills/constants/index.tsx
+++ src/features/Skills/constants/index.tsx
@@ -23,6 +23,7 @@ export const skills = [
   {
     icon: <TypeScriptIcon className={`h-6 w-6 text-blue-500`} />,
     title: "TypeScript",
+    href: "https://www.typescriptlang.org/",
   },
   {
     icon: (
@@ -33,68 +34,84 @@ export const skills = [
       </div>
     ),
     title: "LaTeX",
+    href: "https://www.latex-project.org/",
   },
   {
     icon: <CsharpIcon className={`h-6 w-6`} />,
     title: "C#",
+    href: "https://docs.microsoft.com/en-us/dotnet/csharp/",
   },
   {
     icon: <GraphqlIcon className={`h-6 w-6 text-pink-500`} />,
     title: "GraphQL",
+    href: "https://graphql.org/",
   },
   {
     icon: <JavascriptIcon className={`h-6 w-6 text-yellow-500`} />,
     title: "JavaScript",
+    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
     icon: <ReactIcon className={`h-6 w-6 text-blue-400`} />,
     title: "React",
+    href: "https://reactjs.org/",
   },
   {
     icon: <GatsbyIcon className={`h-6 w-6 text-purple-700`} />,
     title: "Gatsby",
+    href: "https://www.gatsbyjs.com/",
   },
 
   {
     icon: <GitlabIcon className={`h-6 w-6 text-orange-500`} />,
     title: "GitLab CI/CD",
+    href: "https://docs.gitlab.com/ee/ci/",
   },
   {
     icon: <TailwindIcon className={`h-6 w-6 text-teal-400`} />,
     title: "Tailwind CSS",
+    href: "https://tailwindcss.com/",
   },
   {
     icon: <JavaIcon className={`h-6 w-6 text-orange-500`} />,
     title: "Java",
+    href: "https://www.java.com/",
   },
   {
     icon: <DockerIcon className={`h-6 w-6 text-blue-600`} />,
     title: "Docker",
+    href: "https://www.docker.com/",
   },
   {
     icon: <GitIcon className={`h-6 w-6 text-red-500`} />,
     title: "Git",
+    href: "https://git-scm.com/",
   },
 
   {
     icon: <PythonIcon className={`h-6 w-6 text-yellow-500`} />,
     title: "Python",
+    href: "https://www.python.org/",
   },
   {
     icon: <MicrosoftAzureDevopsIcon className={`h-6 w-6 text-blue-400`} />,
     title: "AzureDevOps",
+    href: "https://azure.microsoft.com/en-us/services/devops/",
   },
   {
     icon: <Image alt="Next.js" src={nextJsIcon} className={`h-6 w-6`} />,
     title: "Next.js",
+    href: "https://nextjs.org/",
   },
   {
     icon: <BashIcon className={`h-6 w-6 text-orange-500`} />,
     title: "Bash",
+    href: "https://www.gnu.org/software/bash/",
   },
   {
     icon: <DotNetIcon className={`h-6 w-6 text-purple-500`} />,
     title: "ASP.NET",
+    href: "https://dotnet.microsoft.com/apps/aspnet",
   },
   {
     icon: (
@@ -105,6 +122,7 @@ export const skills = [
       />
     ),
     title: "Cypress",
+    href: "https://www.cypress.io/",
   },
   {
     icon: (
@@ -115,6 +133,7 @@ export const skills = [
       />
     ),
     title: "Nest.js",
+    href: "https://nestjs.com/",
   },
   {
     icon: (
@@ -128,5 +147,6 @@ export const skills = [
       />
     ),
     title: "Proteus 8",
+    href: "https://www.labcenter.com/",
   },
 ];
