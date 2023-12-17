4445570d2e4f763fe4cc59a14be10aacf131e46a
Ali Heydari <ali4heydari@gmail.com>
2023-12-17 13:57:16 +0330

    feat: add git-masters redirect

--- config/next/redirects.js
+++ config/next/redirects.js
@@ -8,4 +8,5 @@ const buildRedirect = (source, destination, permanent = true) => {
 
 module.exports = [
   buildRedirect("/cv", "/static/files/site/ali4heydari-cv.pdf"),
+  buildRedirect("/git-masters", "https://ali4heydari.github.io/git-masters/"),
 ];
