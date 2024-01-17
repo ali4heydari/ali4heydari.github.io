908860dc1dd14c733b4ca6e08ccefb0952da5fd5
Ali Heydari <ali4heydari@gmail.com>
2024-01-17 12:08:30 +0330

    chore: disable outputFileTracing

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  outputFileTracing: false,
   redirects() {
     return redirects;
   },
