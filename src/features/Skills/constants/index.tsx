ac28e368bfea18c2aafc45bca89aca637d172e08
Ali Heydari <ali4heydari@gmail.com>
2025-03-12 22:42:03 +0330

    chore: refactor skill icons to use react-icons library and add new skills

--- src/features/Skills/constants/index.tsx
+++ src/features/Skills/constants/index.tsx
@@ -1,148 +1,177 @@
-import BashIcon from "mdi-react/BashIcon";
-import DockerIcon from "mdi-react/DockerIcon";
-import DotNetIcon from "mdi-react/DotNetIcon";
-import GatsbyIcon from "mdi-react/GatsbyIcon";
-import GitIcon from "mdi-react/GitIcon";
-import GitlabIcon from "mdi-react/GitlabIcon";
-import GraphqlIcon from "mdi-react/GraphqlIcon";
-import CsharpIcon from "mdi-react/LanguageCsharpIcon";
-import JavaIcon from "mdi-react/LanguageJavaIcon";
-import JavascriptIcon from "mdi-react/LanguageJavascriptIcon";
-import PythonIcon from "mdi-react/LanguagePythonIcon";
-import TypeScriptIcon from "mdi-react/LanguageTypescriptIcon";
-import MicrosoftAzureDevopsIcon from "mdi-react/MicrosoftAzureDevopsIcon";
-import ReactIcon from "mdi-react/ReactIcon";
-import TailwindIcon from "mdi-react/TailwindIcon";
-import ProteusLogo from "src/assets/images/logos/proteus-logo.png";
-import cypressIcon from "src/assets/svg/cypress.svg";
-import nestJsIcon from "src/assets/svg/nestjs.svg";
-import nextJsIcon from "src/assets/svg/next-js.svg";
-import Image from "next/image";
+import {
+  FaGitAlt as GitIcon,
+  FaGitlab as GitlabIcon,
+  FaReact as ReactIcon,
+} from "react-icons/fa";
+import { FaJava as JavaIcon } from "react-icons/fa6";
+import {
+  SiGnubash as BashIcon,
+  SiDocker as DockerIcon,
+  SiDotnet as DotNetIcon,
+  SiGatsby as GatsbyIcon,
+  SiGraphql as GraphqlIcon,
+  SiJavascript as JavascriptIcon,
+  SiPython as PythonIcon,
+  SiTypescript as TypeScriptIcon,
+  SiTailwindcss as TailwindIcon,
+  SiCypress as CypressIcon,
+  SiNextdotjs as NextJsIcon,
+  SiNestjs as NestJsIcon,
+  SiProteus as ProteusIcon,
+  SiLatex as LaTeXIcon,
+  SiStorybook as StorybookIcon,
+  SiTestinglibrary as TestingLibraryIcon,
+  SiJest as JestIcon,
+  SiPuppeteer as PuppeteerIcon,
+  SiZod as ZodIcon,
+  SiOpenapiinitiative as OpenApiIcon,
+  SiPrometheus as PrometheusIcon,
+  SiGrafana as GrafanaIcon,
+} from "react-icons/si";
+import { TbBrandCSharp as CsharpIcon } from "react-icons/tb";
+import { VscAzureDevops as MicrosoftAzureDevopsIcon } from "react-icons/vsc";
 
 export const skills = [
   {
     href: "https://www.typescriptlang.org/",
-    icon: <TypeScriptIcon className={`size-6 text-blue-500`} />,
+    icon: <TypeScriptIcon className={"size-6 text-blue-500"} />,
     title: "TypeScript",
   },
   {
     href: "https://www.latex-project.org/",
-    icon: (
-      <div className={`flex size-6 items-center justify-center font-bold`}>
-        <span>
-          L<sup>A</sup>T<sub>E</sub>X
-        </span>
-      </div>
-    ),
+    icon: <LaTeXIcon className="size-6 text-teal-700" />,
     title: "LaTeX",
   },
   {
     href: "https://docs.microsoft.com/en-us/dotnet/csharp/",
-    icon: <CsharpIcon className={`size-6`} />,
+    icon: <CsharpIcon className={"size-6"} />,
     title: "C#",
   },
   {
     href: "https://graphql.org/",
-    icon: <GraphqlIcon className={`size-6 text-pink-500`} />,
+    icon: <GraphqlIcon className={"size-6 text-pink-500"} />,
     title: "GraphQL",
   },
   {
     href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    icon: <JavascriptIcon className={`size-6 text-yellow-500`} />,
+    icon: <JavascriptIcon className={"size-6 text-yellow-500"} />,
     title: "JavaScript",
   },
   {
     href: "https://reactjs.org/",
-    icon: <ReactIcon className={`size-6 text-blue-400`} />,
+    icon: <ReactIcon className={"size-6 text-blue-400"} />,
     title: "React",
   },
   {
     href: "https://www.gatsbyjs.com/",
-    icon: <GatsbyIcon className={`size-6 text-purple-700`} />,
+    icon: <GatsbyIcon className={"size-6 text-purple-700"} />,
     title: "Gatsby",
   },
 
   {
     href: "https://docs.gitlab.com/ee/ci/",
-    icon: <GitlabIcon className={`size-6 text-orange-500`} />,
+    icon: <GitlabIcon className={"size-6 text-orange-500"} />,
     title: "GitLab CI/CD",
   },
   {
     href: "https://tailwindcss.com/",
-    icon: <TailwindIcon className={`size-6 text-teal-400`} />,
+    icon: <TailwindIcon className={"size-6 text-teal-400"} />,
     title: "Tailwind CSS",
   },
   {
     href: "https://www.java.com/",
-    icon: <JavaIcon className={`size-6 text-orange-500`} />,
+    icon: <JavaIcon className={"size-6 text-orange-500"} />,
     title: "Java",
   },
   {
     href: "https://www.docker.com/",
-    icon: <DockerIcon className={`size-6 text-blue-600`} />,
+    icon: <DockerIcon className={"size-6 text-blue-600"} />,
     title: "Docker",
   },
   {
     href: "https://git-scm.com/",
-    icon: <GitIcon className={`size-6 text-red-500`} />,
+    icon: <GitIcon className={"size-6 text-red-500"} />,
     title: "Git",
   },
 
   {
     href: "https://www.python.org/",
-    icon: <PythonIcon className={`size-6 text-yellow-500`} />,
+    icon: <PythonIcon className={"size-6 text-yellow-500"} />,
     title: "Python",
   },
   {
     href: "https://azure.microsoft.com/en-us/services/devops/",
-    icon: <MicrosoftAzureDevopsIcon className={`size-6 text-blue-400`} />,
+    icon: <MicrosoftAzureDevopsIcon className={"size-6 text-blue-400"} />,
     title: "AzureDevOps",
   },
   {
     href: "https://nextjs.org/",
-    icon: <Image alt="Next.js" src={nextJsIcon} className={`size-6`} />,
+    icon: <NextJsIcon className={"size-6"} />,
     title: "Next.js",
   },
   {
     href: "https://www.gnu.org/software/bash/",
-    icon: <BashIcon className={`size-6 text-orange-500`} />,
+    icon: <BashIcon className={"size-6 text-orange-500"} />,
     title: "Bash",
   },
   {
     href: "https://dotnet.microsoft.com/apps/aspnet",
-    icon: <DotNetIcon className={`size-6 text-purple-500`} />,
+    icon: <DotNetIcon className={"size-6 text-purple-500"} />,
     title: "ASP.NET",
   },
   {
     href: "https://www.cypress.io/",
-    icon: (
-      <Image
-        alt="Cypress"
-        src={cypressIcon}
-        className={`size-6 text-gray-300`}
-      />
-    ),
+    icon: <CypressIcon className={"size-6 text-emerald-400"} />,
     title: "Cypress",
   },
   {
     href: "https://nestjs.com/",
-    icon: (
-      <Image alt="Nest.js" src={nestJsIcon} className={`size-6 text-red-500`} />
-    ),
+    icon: <NestJsIcon className={"size-6 text-rose-600"} />,
     title: "Nest.js",
   },
   {
     href: "https://www.labcenter.com/",
-    icon: (
-      <Image
-        src={ProteusLogo}
-        placeholder="blur"
-        height={80}
-        width={80}
-        className={`size-6`}
-        alt={"proteus logo"}
-      />
-    ),
+    icon: <ProteusIcon className={"size-6 text-blue-600"} />,
     title: "Proteus 8",
+  },
+  {
+    href: "https://storybook.js.org/",
+    icon: <StorybookIcon className={"size-6 text-pink-500"} />,
+    title: "Storybook",
+  },
+  {
+    href: "https://testing-library.com/",
+    icon: <TestingLibraryIcon className={"size-6 text-red-600"} />,
+    title: "Testing Library",
+  },
+  {
+    href: "https://jestjs.io/",
+    icon: <JestIcon className={"size-6 text-red-700"} />,
+    title: "Jest",
+  },
+  {
+    href: "https://pptr.dev/",
+    icon: <PuppeteerIcon className={"size-6"} />,
+    title: "Puppeteer",
+  },
+  {
+    href: "https://zod.dev",
+    icon: <ZodIcon className={"size-6 text-blue-600"} />,
+    title: "Zod",
+  },
+  {
+    href: "https://swagger.io/specification/",
+    icon: <OpenApiIcon className={"size-6 text-green-500"} />,
+    title: "OpenAPI",
+  },
+  {
+    href: "https://prometheus.io/",
+    icon: <PrometheusIcon className={"size-6 text-red-500"} />,
+    title: "Prometheus",
+  },
+  {
+    href: "https://grafana.com/",
+    icon: <GrafanaIcon className={"size-6 text-orange-600"} />,
+    title: "Grafana",
   },
 ];
