880d451178238499779c2c9afedc4300c9447ace
Ali Heydari <ali4heydari@gmail.com>
2025-07-19 22:32:18 +0330

    chore: add Providers decorator to Footer story

--- src/components/organism/Footer/Footer.stories.tsx
+++ src/components/organism/Footer/Footer.stories.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Providers from "../../../providers";
 import Footer from "./index";
 import type { StoryFn, Meta } from "@storybook/nextjs";
 
 export default {
   component: Footer,
+  // IDK why I should repeat this, since it is already defined in .storybook/preview.tsx
+  decorators: [
+    (Story) => (
+      <Providers>
+        <Story />
+      </Providers>
+    ),
+  ],
   title: "Organism/Footer",
 } satisfies Meta<typeof Footer>;
 
